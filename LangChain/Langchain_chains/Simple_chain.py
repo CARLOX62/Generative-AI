41984fbe61da16ce9170997f15a87b6fd1378ebf
CARLOX62 <aniketkumarsonu62@gmail.com>
2026-02-25 21:34:41 +0530

    5th commit

---
+++ LangChain/Langchain_chains/Simple_chain.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'cricket'})
+
+print(result)
+
+chain.get_graph().print_ascii()
